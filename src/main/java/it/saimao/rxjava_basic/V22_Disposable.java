40edcfedc0d2c6818f4a04bd2c367939ea33ccb3
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2024-02-12 10:37:13 +0630

    Learnt: V22 - Disposable

---
+++ src/main/java/it/saimao/rxjava_basic/V22_Disposable.java
@@ -0,0 +1,50 @@
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable; /**
+ * This is used when you want to dispose your observables
+ */
+
+Disposable disposable;
+
+void main() {
+    Observable<Integer> observable = Observable.range(0, 100);
+    observable.subscribe(new Observer<>() {
+        @Override
+        public void onSubscribe(@NonNull Disposable d) {
+            disposable = d;
+        }
+
+        @Override
+        public void onNext(@NonNull Integer integer) {
+            System.out.println(integer);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (integer == 10) {
+                System.out.println("Disposing");
+                stopHere();
+            }
+        }
+
+
+        @Override
+        public void onError(@NonNull Throwable e) {
+            System.out.println("On Error");
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println("On Complete");
+        }
+    });
+
+}
+
+void stopHere() {
+
+    if (disposable != null) disposable.dispose();
+
+}
