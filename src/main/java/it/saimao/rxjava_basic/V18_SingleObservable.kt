9b1a11dc4b0d287b6d11454840578e3f27898455
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2024-02-12 08:37:48 +0630

    Learnt: V18 - Single Observable and Observer

--- src/main/java/it/saimao/rxjava_basic/V18_SingleObservable.kt
+++ src/main/java/it/saimao/rxjava_basic/V18_SingleObservable.kt
@@ -1,21 +1,44 @@
 package it.saimao.rxjava_basic
 
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.SingleObserver
+import io.reactivex.rxjava3.disposables.Disposable
 
+/**
+ * Single is used when the observable has to emit only one value
+ * like a response from a network call
+ */
 fun main() {
-    val num = 100
-    Single.create { emitter ->
+    createSingleObservable().subscribe(singleObserver())
+}
+
+const val INTEGER = 19
+
+fun createSingleObservable(): Single<Int> {
+    return Single.create { emitter ->
         try {
-
-            if (num > 100) emitter.onSuccess("YES")
-            else emitter.onSuccess("NO")
+            if (INTEGER > 18) emitter.onSuccess(INTEGER)
+            else emitter.onError(IllegalArgumentException("Integer must be over 18"))
         } catch (e: Exception) {
-
             emitter.onError(e)
         }
-    }.subscribe({
-        println(it)
-    }, {
-        println(it.message)
-    })
+
+    }
+}
+
+fun singleObserver(): SingleObserver<Int> {
+    return object : SingleObserver<Int> {
+        override fun onSubscribe(d: Disposable) {
+            println("On Subscribe")
+        }
+
+        override fun onError(e: Throwable) {
+            println("On Error - ${e.message}")
+        }
+
+        override fun onSuccess(t: Int) {
+            println("On Success - $t")
+        }
+
+    }
 }
